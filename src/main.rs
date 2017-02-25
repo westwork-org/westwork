22bc097c89ecd78e0ee60aebca39bdf147710ee7
Ross Schulman <ross@NA-LM-0301.fios-router.home>
2017-02-24 19:52:49 -0500

    Generate a random password and hash it for LDAP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 extern crate mustache;
 extern crate rustc_serialize;
+extern crate rand;
 
+use std::io::prelude::*;
 use std::io;
 use std::path::Path;
+use std::process::{Command, Stdio};
+use rand::Rng;
 
 #[derive(RustcEncodable)]
 struct Instance {
     domain: String,
     admin_user: String,
     admin_name: String,
+    root_pass: String,
 }
 
 fn main () {
+    // Generate a random password then hash it for inserting into ldap
+    // TODO: We should only do this the first time we're started up and save the results
+    let clear_passwd: String = rand::thread_rng().gen_ascii_chars().take(20).collect();
+    let output = match Command::new("mkpasswd").arg("-m").arg("SHA512").arg("-s").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn() {
+        Ok(process) => process,
+        Err(error) => panic!("Unable to generate password, do you have mkpasswd installed? {}", error),
+    };
+    match output.stdin.unwrap().write_all(clear_passwd.as_bytes()) {
+        Ok(_) => {},
+        Err(error) => panic!("Could not write password to mkpasswd. {}", error),
+    }
+    let mut passwd = String::new();
+    match output.stdout.unwrap().read_to_string(&mut passwd) {
+        Ok(_) => {},
+        Err(error) => panic!("couldn't read mkpasswd stdout: {}", error),
+    }
+    passwd = "{SHA512}" + &passwd;
+
     let mut input = String::new();
     println!("Input your name, username, and domain separated by commas.");
     match io::stdin().read_line(&mut input) {
@@ -22,7 +45,8 @@ fn main () {
     let this_instance = Instance {
         domain: split[2].trim().to_string(),
         admin_user: split[1].to_string(),
-        admin_name: split[0].to_string()
+        admin_name: split[0].to_string(),
+        root_pass: passwd,
     };
 
     let compose_template = match mustache::compile_path(Path::new("docker-compose.yml.mustache")) {
