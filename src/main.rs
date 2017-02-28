bcbc79b726b6a42aa92befe1b5caa8123b021a47
Ross Schulman <ross@rbs.io>
2017-02-27 21:39:32 -0500

    Clear out the deadweight

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,4 @@
 extern crate mustache;
-extern crate rustc_serialize;
-extern crate rand;
-
-use std::io::prelude::*;
-use std::io;
-use std::fs::File;
-use std::path::Path;
-use rand::Rng;
-
-#[derive(RustcEncodable)]
-struct Instance {
-    domain: String,
-    admin_user: String,
-    admin_name: String,
-    root_pass: String,
-}
 
 fn main () {
-    // Generate a random password then hash it for inserting into ldap
-    // TODO: We should only do this the first time we're started up and save the results
-    // let clear_passwd: String = rand::thread_rng().gen_ascii_chars().take(20).collect();
-    // passwd = passwd.replace('$', "$$");
-    // passwd = passwd.trim().to_string();
-    let passwd = "testpassword".to_string();
-
-    let mut input = String::new();
-    println!("Input your name, username, and domain separated by commas.");
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {},
-        Err(error) => println!("Error: {}", error)
-    }
-    let split: Vec<&str> = input.split(',').collect();
-    let this_instance = Instance {
-        domain: split[2].trim().to_string(),
-        admin_user: split[1].to_string(),
-        admin_name: split[0].to_string(),
-        root_pass: passwd,
-    };
-
-    let compose_template = match mustache::compile_path(Path::new("docker-compose.yml.mustache")) {
-        Ok(template) => template,
-        Err(error) => panic!("Could not find docker-compose template: {}", error),
-    };
-    let mut f = File::create("docker-compose.yml").expect("Could not open docker-compose.yml file. Check permissions?");
-    compose_template.render(&mut f, &this_instance);
 }
