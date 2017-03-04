efb25dcd605bc314086ca8d55b9ec4ea91617853
Ross Schulman <ross@rbs.io>
2017-03-04 13:30:10 -0500

    Client will happen in another repo.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 extern crate websocket;
 
-use std::fs::File;
+use std::path::Path;
 use std::thread;
 use websocket::{Server, Message};
 
-mod configure;
-
 fn main () {
-    let mut configured = true;
-    let config_file = Path::new("/etc/westwork/westwork.conf");
-    if config_file.exists() {
-        // Read it into a struct somehow
-    } else {
-        configured = false;
-    }
 
     let server = match Server::bind("127.0.0.1:8000") {
         Ok(connection) => connection,
@@ -25,13 +16,7 @@ fn main () {
             let request = new_conn.unwrap().read_request().unwrap();
             let response = request.accept(); // Form a response
             let mut client = response.send().unwrap(); // Send the response
-
-            if configured {
-                ClientConnection::new(client);
-            } else {
-                configure::Configure::with_connection(client);
-            }
-            
+            ClientConnection::new(client);
         });
     }
 }
