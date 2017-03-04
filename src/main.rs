d1e821e56bc24c033be1926a3c92f85c14df3b0c
Ross Schulman <ross@rbs.io>
2017-03-03 21:15:32 -0500

    Websocket scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
+extern crate websocket;
 
 use std::fs::File;
+use std::thread;
+use websocket::{Server, Message};
+
+mod configure;
 
 fn main () {
-    rocket::ignite().mount("/", routes![index, get_css]).launch();
+    let mut configured = true;
+    let config_file = Path::new("/etc/westwork/westwork.conf");
+    if config_file.exists() {
+        // Read it into a struct somehow
+    } else {
+        configured = false;
+    }
+
+    let server = match Server::bind("127.0.0.1:8000") {
+        Ok(connection) => connection,
+        Err(e) => panic!("Could not bind websocket server: \{:?}", e)
+    };
+
+    for new_conn in server {
+        thread::spawn(move || {
+            let request = new_conn.unwrap().read_request().unwrap();
+            let response = request.accept(); // Form a response
+            let mut client = response.send().unwrap(); // Send the response
+
+            if configured {
+                ClientConnection::new(client);
+            } else {
+                configure::Configure::with_connection(client);
+            }
+            
+        });
+    }
 }
