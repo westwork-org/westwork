8e29d6c8a31de3bcb50a369e2b79f0f304559ac2
Ross Schulman <ross@rbs.io>
2017-03-05 13:03:06 -0500

    Sketch out main receive loop.

--- client_connection/client_connection.rs
+++ client_connection/client_connection.rs
@@ -1,2 +1,51 @@
-struct ClientConnection;
+struct ClientConnection {
+    authed_used: Option<User>,
+}
 
+impl ClientConnection {
+    pub fn new (handle: websocket::client::Client) {
+        // Loop to listen for messages
+        // We will probably have to also spawn a second thread to send eventually
+        let (mut sender, mut receiver) = client.split();
+
+        // TODO: Make a channel here
+
+        thread::spawn(move || {
+            for message in receiver.incoming_messages() {
+                let message: Message = message.unwrap();
+                // Types of messages:
+                // 1. Hello (first or reestablished connection) w/ auth token
+                // 2. Messages
+                // 3. Calendar
+                // 4. Files(?)
+                // 5. Logout
+                match message.opcode() {
+                    Type::Binary => {
+                        println!("Got a binary websocket message??");
+                        continue;
+                    }
+                    Type::Pong => {
+                        // Right now I don't think we have to do anything with this.
+                    }
+                    Type::Ping => {
+                        send_loop.send(message.into_pong().unwrap()); // This shouldn't ever fail.
+                    }
+                    Type::Close => {
+                        println!("Received close from client, shutting down thread.");
+                        send_loop.send("SHUTDOWN");
+                        receiver.shutdown_receiver();
+                        break;
+                    }
+                    Type::Text => {
+                        
+                    }
+
+
+                }
+            }
+        })
+        loop {
+            // Sender loop here.
+        }
+    }
+}
