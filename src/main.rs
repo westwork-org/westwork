2a3091812152dd6f1de7363430ffbf1ff3ae7488
Ross Schulman <ross@rbs.io>
2017-03-28 15:12:19 +0200

    Wipe old approach

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-extern crate websocket;
 
-use std::path::Path;
-use std::thread;
-use websocket::{Server, Message};
-
-mod client_connection;
 
 fn main () {
 
-    let server = match Server::bind("127.0.0.1:8000") { 
-        // Of course this should be a secure websocket eventually
-        Ok(connection) => connection,
-        Err(e) => panic!("Could not bind websocket server: \{:?}", e)
-    };
-
-    for new_conn in server {
-        thread::spawn(move || {
-            let request = new_conn.unwrap().read_request().unwrap();
-            let response = request.accept(); // Form a response
-            let mut client = response.send().unwrap(); // Send the response
-            client_connection::ClientConnection::new(client);
-        });
-    }
 }
