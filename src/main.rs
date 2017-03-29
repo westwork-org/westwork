1fc73b9beefc0cd2b2031ef62548035c1a0d0a9a
Ross Schulman <ross@rbs.io>
2017-03-29 14:19:31 +0200

    Load config file or start new one

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+extern crate config;
+use config::{Config, File, FileFormat};
 
+const settings_path = "/data/conf/westwork/settings.yaml"
 
 fn main () {
+    let mut conf = config::Config::new();
+
+    if (Path::new(settings_path).exists()) {
+        conf.merge(File::new(settings_path), FileFormat::Yaml).unwrap();
+    } else {
+        conf.set(&"configured", false);
+    }
+
+    
+    
 
 }
