824968f7df129013b6f236ca5097afece1f9da3b
Ross Schulman <ross@rbs.io>
2017-03-30 16:04:52 +0200

    Fix bugs to get add_name to compile
    Mostly missing some libraries in Cargo.toml, but also moving the Config object into a Mutex for safe access across threads.

--- src/bootstrap/mod.rs
+++ src/bootstrap/mod.rs
@@ -1,3 +1,9 @@
+use rocket::State;
+use config::Config;
+use rocket_contrib::{JSON, Value};
+use std::collections::HashMap;
+use WestworkConf;
+
 // TODO: How to handle names that don't follow the First Last template
 #[derive(Deserialize)]
 struct Name {
@@ -5,11 +11,12 @@ struct Name {
     last: String
 }
 
-#[post("/add_name", format = "application/json" data = "<name>")]
-fn add_name(name: JSON<Name>, conf: State<Config>) -> JSON<Value> {
+#[post("/add_name", format = "application/json", data = "<name>")]
+fn add_name(name: JSON<Name>, conf: State<WestworkConf>) -> JSON<Value> {
     // Receive the new user's name and insert into the state
-    conf.set(&"first_name", name.first);
-    conf.set(&"last_name", name.last);
+    let mut this_conf = conf.lock().expect("Failed to lock conf");
+    this_conf.set(&"first_name", name.0.first);
+    this_conf.set(&"last_name", name.0.last);
     let mut resp = HashMap::new();
     resp.insert("success", "true");
     resp.insert("next_html", "<span id='boot-ques'>What would you like your username to be?</span>\
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,35 @@
 
 extern crate config;
 extern crate rocket;
+extern crate serde_json;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
 use config::{Config, File, FileFormat};
+use rocket_contrib::{JSON, Value};
+use rocket::State;
+use std::collections::HashMap;
+use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 mod bootstrap;
 
-const settings_path: String = "/data/conf/westwork/settings.yaml"
+const settings_path: &str = "/data/conf/westwork/settings.yaml";
+
+type WestworkConf = Arc<Mutex<Config>>;
 
 fn main () {
-    let mut conf = config::Config::new();
-
-    if (Path::new(settings_path).exists()) {
-        conf.merge(File::new(settings_path), FileFormat::Yaml).unwrap();
+    let conf = Arc::new(Mutex::new(config::Config::new()));
+    let cloned_conf = conf.clone();
+    let mut this_conf = cloned_conf.lock().expect("Failed to lock config.");
+    if Path::new(settings_path).exists() {
+        this_conf.merge(File::new(settings_path, FileFormat::Yaml)).unwrap();
     } else {
-        conf.set(&"configured", false);
+        this_conf.set(&"configured", false);
     }
+    drop(this_conf);
 
 
-    rocket::ignite().mount("/", routes!([bootstrap::add_name])).manage(conf).launch();
+    rocket::ignite().mount("/", routes![bootstrap::add_name]).manage(conf).launch();
 
 }
