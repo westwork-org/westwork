2ffd47ce1dfefd9c83afa053bc0c158a6e466051
Ross Schulman <ross@rbs.io>
2017-04-03 15:51:19 -0400

    Create add_username route for bootstrap

--- src/bootstrap/mod.rs
+++ src/bootstrap/mod.rs
@@ -27,3 +27,20 @@ fn add_name(name: JSON<Name>, conf: State<WestworkConf>) -> JSON<Value> {
                               </div>");
     JSON(json!(resp))
 }
+
+#[post("/add_username", format = "application/json", data = "<username>")]
+fn add_username(username: JSON<Username>, conf: State<WestworkConf>) -> JSON<Value> {
+    let mut this_conf = conf.lock().expect("Failed to lock conf");
+    this_conf.set(&"username", username.0.username);
+    let mut resp = HashMap::new();
+    resp.insert("success", "true");
+    resp.insert("next_html", "<span id='boot-ques'>Ok, let's find a domain name for you.</span>\
+                              <span id='boot-desc'>(That's the part of your email address that comes after the '@' symbol.)</span>\
+                              <span id='boot-desc'>Give me a name you'd like and I'll see if I can find it for you.</span>\
+                              <div id='boot-form'>\
+                              <form action='search_domain'>\
+                                  <input type='text' placeholder='example.com'>\
+                              </form>\
+                              </div>");
+    JSON(json!(resp))
+}
