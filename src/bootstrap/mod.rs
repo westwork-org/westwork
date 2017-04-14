4de3ce0eb05bcefcb7913f177a8f65fe8edc20a7
Ross Schulman <ross@rbs.io>
2017-04-14 13:53:43 -0400

    Restrict unsafe portions

--- src/bootstrap/mod.rs
+++ src/bootstrap/mod.rs
@@ -13,9 +13,9 @@ struct Name {
     first: String,
     last: String
 }
-static IW_AUTH_WPA_VERSION_DISABLED: u8 =   0x00000001
-static IW_AUTH_WPA_VERSION_WPA: u8 =		0x00000002
-static IW_AUTH_WPA_VERSION_WPA2: u8 =     	0x00000004
+static IW_AUTH_WPA_VERSION_DISABLED: u8 =   0x00000001;
+static IW_AUTH_WPA_VERSION_WPA: u8 =		0x00000002;
+static IW_AUTH_WPA_VERSION_WPA2: u8 =     	0x00000004;
 
 #[derive(Deserialize)]
 struct WifiNetwork{
@@ -45,51 +45,49 @@ fn wifi_list() -> JSON<Value> {
     // In order to join the user's local wireless network, we have to give them a list of available networks.
     // There's no Rust library that does this to date, so we have to call out to the iw library.
     // It is probably worth pulling this out into its own mod eventually, or perhaps its own crate.
-    unsafe {
-        // First get an iw socket.
-        let sock = iw_socket_open();
-        let interface_name = CString::new("wlan0").unwrap();
-        let range: iwrange;
-        let head: wireless_scan_head;
-        if iw_get_range_info(sock, interface_name, &range) < 0 {
-            // We have to make this call in order to get the version of the library on the computer
-            let mut resp = HashMap::new();
-            resp.insert("success", "false");
-            resp.insert("error", "Could not retrieve wireless network list.");
-            JSON(json!(resp))
-        }
-        if iw_scan(sock, interface_name, range.we_version_compiled, &head) <0 {
-            // This is the actual scan call that fills in the `head` struct with information about the visible networks.
-            let mut resp = HashMap::new();
-            resp.insert("success", "false");
-            resp.insert("error", "Could not retrieve wireless network list.");
-            JSON(json!(resp))
-        }
-        result = head.result;
-        let mut list = Vec::new();
-        while result != ptr::null {
-            // The scan results are a linked list of structs with a bunch of information about each network
-            // The type of encryption is encoded in a bitflag called `key_flags` which we check by doing
-            // a bitwise and against the known bitflags.
-            let answer = if result.b.key_flags & IW_AUTH_WPA_VERSION_DISABLED > 0 {
-                "None".to_string()
-            } else if result.b.key_flags & IW_AUTH_WPA_VERSION_WPA > 0 {
-                "WPA".to_string()
-            } else if result.b.key_flags & IW_AUTH_WPA_VERSION_WPA2 > 0 {
-                "WPA2".to_string()
-            } ;
-            list.push(WifiNetwork {
-                SSID: result.b.essid.to_string(),
-                encryption: answer, // TODO Figure out how to get encryption type from `result`
-                strength: result.stats
-            });
-            result = result.next;
-        }
-        resp.insert("success", "false");
-        resp.insert("networks", json!(list));
-        JSON(json!(resp))
 
+    // First get an iw socket.
+    let sock = unsafe { iw_socket_open() };
+    let interface_name = CString::new("wlan0").unwrap();
+    let range: iwrange;
+    let head: wireless_scan_head;
+    if unsafe { iw_get_range_info(sock, interface_name, &range) } < 0 {
+        // We have to make this call in order to get the version of the library on the computer
+        let mut resp = HashMap::new();
+        resp.insert("success", "false");
+        resp.insert("error", "Could not retrieve wireless network list.");
+        JSON(json!(resp))
     }
+    if unsafe { iw_scan(sock, interface_name, range.we_version_compiled, &head) } <0 {
+        // This is the actual scan call that fills in the `head` struct with information about the visible networks.
+        let mut resp = HashMap::new();
+        resp.insert("success", "false");
+        resp.insert("error", "Could not retrieve wireless network list.");
+        JSON(json!(resp))
+    }
+    result = head.result;
+    let mut list = Vec::new();
+    while result != ptr::null {
+        // The scan results are a linked list of structs with a bunch of information about each network
+        // The type of encryption is encoded in a bitflag called `key_flags` which we check by doing
+        // a bitwise and against the known bitflags.
+        let answer = if result.b.key_flags & IW_AUTH_WPA_VERSION_DISABLED > 0 {
+            "None".to_string()
+        } else if result.b.key_flags & IW_AUTH_WPA_VERSION_WPA > 0 {
+            "WPA".to_string()
+        } else if result.b.key_flags & IW_AUTH_WPA_VERSION_WPA2 > 0 {
+            "WPA2".to_string()
+        } ;
+        list.push(WifiNetwork {
+            SSID: result.b.essid.to_string(),
+            encryption: answer, // TODO Figure out how to get encryption type from `result`
+            strength: result.stats
+        });
+        result = result.next;
+    }
+    resp.insert("success", "false");
+    resp.insert("networks", json!(list));
+    JSON(json!(resp))
 }
 
 #[post("/add_name", format = "application/json", data = "<name>")]
